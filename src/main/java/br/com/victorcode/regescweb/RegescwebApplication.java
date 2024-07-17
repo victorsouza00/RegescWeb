53acae277523a576a630a79ed9afceb0dddac5e7
VictorSouza <victorsouza183@gmail.com>
2024-07-17 19:40:30 -0300

    Inicial Commit

---
+++ src/main/java/br/com/victorcode/regescweb/RegescwebApplication.java
@@ -0,0 +1,13 @@
+package br.com.victorcode.regescweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegescwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegescwebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/victorcode/regescweb/controllers/hello.java
@@ -0,0 +1,12 @@
+package br.com.victorcode.regescweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class hello {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello.html";
+    }
+}
