452956fca558a801e0d467a1dcc1525a8cf4e3ad
VictorSouza <victorsouza183@gmail.com>
2024-07-18 18:29:55 -0300

    Adição do controller de teste

--- src/main/java/br/com/victorcode/regescweb/controllers/hello.java
+++ src/main/java/br/com/victorcode/regescweb/controllers/hello.java
@@ -1,12 +1,29 @@
 package br.com.victorcode.regescweb.controllers;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class hello {
+
     @GetMapping("/hello")
-    public String hello(){
+    public ModelAndView hello(){
+        ModelAndView mv = new ModelAndView("hello");
+        mv.addObject("nome","Maria");
+        return mv;
+    }
+    @GetMapping("/hello-servlet")
+    public String hello(HttpServletRequest request){
+        request.setAttribute("nome","Victor");
+        return "Hello.html";
+    }
+    @GetMapping("/hello-model")
+    public String hello(Model model){
+        model.addAttribute("nome","Luiz");
         return "Hello.html";
     }
 }
